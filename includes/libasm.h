01b7435610a59af01d8ea43619f255744e52dff9
issac nam <simian114@gmail.com>
2020-06-04 17:15:30 +0900

    2020. 06. 04. Header

--- includes/libasm.h
+++ includes/libasm.h
@@ -9,5 +9,7 @@ ssize_t		ft_write(int fd, const void *buf, size_t nbyte);
 size_t		ft_strlen(const char *s);
 size_t		ft_read(int fd, void *buf, size_t count);
 int			ft_strcmp(const char *s1, const char *s2);
+char		*ft_strcpy(char *dest, const char *src);
+char		*ft_strdup(const char *s);
 
 #endif
