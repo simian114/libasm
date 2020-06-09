68121dfd1d67994a9806bbaa5c933eb1c67561e5
Sanghyeok Nam <sanam@c1r3s2.42seoul.kr>
2020-06-09 17:45:23 +0900

    2020. 06. 09. Mac...

--- includes/libasm.h
+++ includes/libasm.h
@@ -5,6 +5,15 @@
 # include <unistd.h>
 # include <errno.h>
 
+typedef struct		s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+int			ft_list_size(t_list *begin_list);
+void		ft_list_push_front(t_list **begin_list, void *data);
+
 ssize_t		ft_write(int fd, const void *buf, size_t nbyte);
 size_t		ft_strlen(const char *s);
 size_t		ft_read(int fd, void *buf, size_t count);
